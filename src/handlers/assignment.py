e9596e408eb19ca560cf7afac885f54de72f6ce5
GaubergEONE <127289500+GaubergEONE@users.noreply.github.com>
2025-05-01 15:25:20 +0300

    await state.set_state(None) в finish_assignment_handler

--- src/handlers/assignment.py
+++ src/handlers/assignment.py
@@ -202,7 +202,7 @@ async def finish_assignment_handler(call: CallbackQuery, state: FSMContext):
                 data['lessons_page'], data['assignments']),
             disable_web_page_preview=True
         )
-        await state.reset_state(with_data=False)
+        await state.set_state(None)
         return
 
     # Подготовка массивов для отправки
